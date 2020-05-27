e92864677266dd4189b538dbacdb8a46dd1b482d
Haguro-zz <hugo.ols@outlook.com>
2020-05-27 20:54:11 -0300

    Rede

--- rede.sh
+++ rede.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo -e "\n   Informações de Rede   "
-echo -e "=========================\n"
+echo -e "\nEndereços de Rede IPv4"
+echo
 
-ifconfig | grep "UP" | cut -d" " -f1
-ifconfig | grep "inet"
+ip a | grep "UP" | cut -d" " -f2 > placas
+ip a | grep "inet " | cut -d" " -f6 > enderecos
+
+paste placas enderecos
+rm placas enderecos
+echo

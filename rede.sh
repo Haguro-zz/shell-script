c19f34fd2a544de35ddfd807749703d6808c4fae
Haguro-zz <hugo.ols@outlook.com>
2020-05-28 10:06:27 -0300

    Rede

--- rede.sh
+++ rede.sh
@@ -7,5 +7,14 @@ ip a | grep "UP" | cut -d" " -f2 > placas
 ip a | grep "inet " | cut -d" " -f6 > enderecos
 
 paste placas enderecos
-rm placas enderecos
+
+echo -e "\nEndereços de Rede IPv6"
+echo
+
+ip a | grep "UP" | cut -d" " -f2 > placas6
+ip a | grep "inet6" | cut -d" " -f6 > enderecos6
+
+paste placas6 enderecos6
+
+rm placas enderecos placas6 enderecos6
 echo
--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 
 apt update
 apt upgrade -y
+apt dist-upgrade -y
 apt autoremove -y
 
 echo -e "\nSistema atualizado!\n"

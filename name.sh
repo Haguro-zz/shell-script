b3a8fa984e346f11848e9b46e638bd7d5c780bb9
Hugo <hugo.ols@outlook.com>
2022-05-11 14:38:57 -0300

    Update name.sh

--- name.sh
+++ name.sh
@@ -8,8 +8,8 @@ else
         version_SO=$(cat /etc/issue | grep cut -d '\' -f1)
 fi
 
-echo -e "\n   \033[1mDados da Máquina"
-echo -e "=======================\033[m\n"
+echo "\n   \033[1mDados da Máquina"
+echo "=======================\033[m\n"
 
 printf "Data: \033[1;31m"
 date "+%A, %d/%m/%Y - %H:%M"
@@ -17,20 +17,20 @@ echo -e "\033[m---------------------------------"
 
 printf "Host: \033[1;31m"
 uname -a | cut -d" " -f2
-echo -e "\033[m---------------------------------"
+echo "\033[m---------------------------------"
 
 printf "Versão: \033[1;31m"
 echo "$version_SO"
-echo -e "\033[m---------------------------------"
+echo "\033[m---------------------------------"
 
 printf "Kernel: \033[1;31m"
 uname -a | cut -d" " -f6,7
-echo -e "\033[m---------------------------------"
+echo "\033[m---------------------------------"
 
-echo -e "Usuários conectados: \033[1;31m\n"
+echo "Usuários conectados: \033[1;31m\n"
 who -H
-echo -e "\033[m---------------------------------"
+echo "\033[m---------------------------------"
 
 printf "Processador: \033[1;31m"
 head /proc/cpuinfo | grep "model name" | cut -d" " -f3-20
-echo -e "\033[m---------------------------------"
+echo "\033[m---------------------------------"

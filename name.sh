a24659e9cbb275ff69937913e0f98d4269e51b54
Hugo <hugo.ols@outlook.com>
2022-05-11 14:50:34 -0300

    Update name.sh

--- name.sh
+++ name.sh
@@ -8,29 +8,29 @@ else
         version_SO=$(cat /etc/issue | awk '{print $1,$2,$3}')
 fi
 
-echo "\n \033[1mDados da Máquina"
-echo "=======================\033[m\n"
+printf "\n \033[1mDados da Máquina"
+printf "=======================\033[m\n"
 
 printf "Data: \033[1;31m"
 date "+%A, %d/%m/%Y - %H:%M"
-echo "\033[m---------------------------------"
+printf "\033[m---------------------------------"
 
 printf "Host: \033[1;31m"
 uname -a | cut -d" " -f2
-echo "\033[m---------------------------------"
+printf "\033[m---------------------------------"
 
 printf "Versão: \033[1;31m"
 echo "$version_SO"
-echo "\033[m---------------------------------"
+printf "\033[m---------------------------------"
 
 printf "Kernel: \033[1;31m"
 uname -a | cut -d" " -f6,7
-echo "\033[m---------------------------------"
+printf "\033[m---------------------------------"
 
 echo "Usuários conectados: \033[1;31m\n"
 who -H
-echo "\033[m---------------------------------"
+printf "\033[m---------------------------------"
 
 printf "Processador: \033[1;31m"
 head /proc/cpuinfo | grep "model name" | cut -d" " -f3-20
-echo "\033[m---------------------------------"
+printf "\033[m---------------------------------"
